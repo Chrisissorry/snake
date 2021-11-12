c1e914ec8df4fa6c8562f011b8a18fd584c12910
Marco Schwarz <marco.schwarz@atp-autoteile.de>
2021-11-12 09:55:19 +0100

    WIP

---
+++ UnitTests/Snake.test.js
@@ -0,0 +1,11 @@
+﻿import {Snake} from "../modules/Snake";
+
+test("Initialize Snake", () => {
+    const snake = new Snake();
+    expect(snake).toBeTruthy();
+})
+
+test("Snake is drawn", () => {
+    const snake = new Snake();
+    snake.draw();
+})
--- modules/Apple.js
+++ modules/Apple.js
@@ -1,35 +1,7 @@
-import {
-    startingX, 
-    startingY,
-} from "./Snake.js";
-
-import {
-    ctx,
-    gameScreenHeight,
-    gameScreenWidth,
-} from "../Start.js";
-
-export let applePositionX;
-export let applePositionY;
-
-export function isAppleEaten() {
-    return startingX === applePositionX && startingY === applePositionY;
-}
-
-export function initApple() {
-    applePositionX = getRandomInt(0,gameScreenWidth);
-    applePositionY = getRandomInt(0,gameScreenHeight);
-}
-
-export function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(((Math.random() * (max - min +1)) + min)/10)*10;
-}
-
-export function rect(x,y,width,height) {
-    ctx.beginPath();
-    ctx.rect(x,y,width,height);
-    ctx.closePath();
-    ctx.fill();
+﻿export const Apple = () => {
+    let appleWide = 10;
+    let appleHeight = 10;
+    let randomAppleX;
+    let randomAppleY;
+    let generateNewApple;
 }
