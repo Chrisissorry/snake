b9ff591d372231377d6e29f0010bb36e4a726ab2
Marco Schwarz <marco.schwarz@atp-autoteile.de>
2021-11-22 14:30:35 +0100

    Implementet some functions and tests

---
+++ UnitTests/Apple.test.js
@@ -0,0 +1,24 @@
+﻿import {Apple} from "../modules/Apple";
+
+describe("Function randomFood", () => {
+    
+    const apple = new Apple();
+    
+    test("Init random Apple", () => {
+        expect(apple.randomFood).toBeTruthy();
+    })
+    
+    test("randomFood returns something", () => {
+        jest.spyOn(apple, "randomFood");
+        apple.randomFood(100, 200);
+        expect(apple.randomFood).toReturn();
+    })
+    
+    test("randomFood is between min and max", () => {
+        jest.retryTimes(100);
+        jest.spyOn(apple, "randomFood")
+        apple.randomFood(100, 200);
+        expect(apple.randomApple).toBeGreaterThan(100)
+        expect(apple.randomApple).toBeLessThan(200)
+    })
+})
