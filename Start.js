63c75d8edb89d12c10620b925a37ced5cf65cf59
Marco Schwarz <marco.schwarz@atp-autoteile.de>
2021-11-23 15:16:45 +0100

    Implemented some Funktions with UI tests

---
+++ Start.js
@@ -0,0 +1,11 @@
+﻿export class Start {
+    constructor() {
+        this.startscreen = document.getElementById("startscreen");
+        this.titleimage = document.getElementById("titleimage");
+        this.easy = document.getElementById("easy");
+        this.medium = document.getElementById("medium");
+        this.hard = document.getElementById("hard");
+        this.canvascontainer = document.getElementById("canvascontainer")
+    }
+    
+}
