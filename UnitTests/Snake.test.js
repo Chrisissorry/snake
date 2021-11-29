15868d3af450c5f55647d9385f3ca860bab888ae
Marco Schwarz <marco.schwarz@atp-autoteile.de>
2021-11-29 16:10:41 +0100

    fixed some issues on the Tests

--- UnitTests/Snake.test.js
+++ UnitTests/Snake.test.js
@@ -3,28 +3,28 @@
 describe("function checkDirection", () => {
     
     test("checkDirection right is working", () => {
-        const snake = new Snake("up", 0, -10)
+        const snake = new Snake("right", 0, -10)
         snake.checkDirection()
         expect(snake.dx).toBe(10)
         expect(snake.dy).toBe(0)
     })
 
     test("checkDirection left is working", () => {
-        const snake = new Snake("up", 0, -10)
+        const snake = new Snake("left", 0, -10)
         snake.checkDirection()
         expect(snake.dx).toBe(-10)
         expect(snake.dy).toBe(0)
     })
 
     test("checkDirection down is working", () => {
-        const snake = new Snake("left", -10, 0)
+        const snake = new Snake("down", -10, 0)
         snake.checkDirection()
         expect(snake.dx).toBe(0)
         expect(snake.dy).toBe(10)
     })
 
     test("checkDirection up is working", () => {
-        const snake = new Snake("left", -10, 0)
+        const snake = new Snake("up", -10, 0)
         snake.checkDirection()
         expect(snake.dx).toBe(0)
         expect(snake.dy).toBe(-10)
